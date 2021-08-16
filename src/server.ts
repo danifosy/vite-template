1c6a06a6522f0b058e56f2428fc5a332b07cfe17
danifosy <mail@daniha.de>
2021-08-16 15:13:10 +0200

    Create react starter

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello');
 });
 
