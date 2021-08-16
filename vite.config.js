eedac10cb21b10c67ef6ac2589020c650ac86b5f
danifosy <mail@daniha.de>
2021-08-16 13:56:06 +0200

    Fast refresh for react in vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
+  plugins: [reactRefresh()],
   build: {
     outDir: 'dist/app',
   },
