4234e816f81de33eed6ac47d3c98165a8ef6307e
danifosy <mail@daniha.de>
2021-08-16 12:19:53 +0200

    Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Nightmare King');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}!`);
+});
