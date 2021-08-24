9f4b147521d375eaceb603a790fa8ff9681b1953
danifosy <mail@daniha.de>
2021-08-24 14:02:40 +0200

    Install concurrently

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.get('/api/hello', (_request, response) => {
+  response.send('Hello API!');
+});
 
 app.use('/storybook', express.static('dist/storybook'));
 
 app.use(express.static('dist/app'));
 
 app.listen(port, () => {
-  console.log(`Server is listening at port ${port}!`);
+  console.log(`Server listening on port ${port}!`);
 });
